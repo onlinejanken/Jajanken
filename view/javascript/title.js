389c0669abe1c379007fcf68e532d341abc70dd0
oreotabetai <s17t213@stu.kagawa-u.ac.jp>
2019-04-24 23:25:36 +0900

    拡張子をejsに変更。静的ファイルをpublicに配置。 #5

--- view/javascript/title.js
+++
@@ -1,8 +0,0 @@
-var b1 = document.getElementById("b1").style.display = "none";
-window.onload = function(){
-    b1.style.display="none";
-}
-function Click(){
-    username = window.prompt("ユーザめいをにゅうりょくしてください");
-    b1.style.display ="block";
-}
