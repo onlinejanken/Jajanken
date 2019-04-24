c337c4ce3ca2d034b515aee2b63901bf663ff551
oreotabetai <s17t213@stu.kagawa-u.ac.jp>
2019-04-24 22:22:26 +0900

    viewのパッケージを追加しました #5

---
+++ view/javascript/title.js
@@ -0,0 +1,8 @@
+var b1 = document.getElementById("b1").style.display = "none";
+window.onload = function(){
+    b1.style.display="none";
+}
+function Click(){
+    username = window.prompt("ユーザめいをにゅうりょくしてください");
+    b1.style.display ="block";
+}
