ed65f805d8f26c1a7eed83f4cf8535907cf5b5b6
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2019-04-26 21:08:21 +0900

    スペルミスを修正 #7

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const controller = require('./controller/controller.js');
 
-app.set('vew engin', 'ejs');
+app.set('veiw engin', 'ejs');
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/', controller);
 
