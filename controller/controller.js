5c902945832a7d79cdeff963710af2a10e1ec6a3
git-Ktu <brdm82a1@gmail.com>
2019-04-29 01:27:13 +0900

    controllerの実装 #4

--- controller/controller.js
+++ controller/controller.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/',(req,res) => {
+    res.render('title');
+});
+router.post('/admins',(req,res) => {
+    res.render('admins');
+});
+router.post('/participants', (req,res) => {
+    res.render('participants');
+});
+
 module.exports = router;
