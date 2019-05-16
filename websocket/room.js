7a763e82f128325ec8c3d61c5c4aecf964831f38
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2019-05-17 01:11:11 +0900

    ルームの参加とルーム人数の送信を実装 #28

--- websocket/room.js
+++ websocket/room.js
@@ -1,6 +1,11 @@
 module.exports = (app, http, socket) => {
     const io = socket.listen(http);
     io.sockets.on("connection", (socket) => {
+
+        socket.on('enter', (roomId) => {
+            socket.join(roomId);
+            io.to(roomId).emit('roomPeople', io.sockets.client(roomId).length);  // ルームの参加人数を送信
+        });
         //メッセージ送信（送信者にも送られる）
         socket.on("C_to_S_message", (data) => {
 
@@ -8,12 +13,11 @@ module.exports = (app, http, socket) => {
 
         //ブロードキャスト（送信者以外の全員に送信）
         socket.on("C_to_S_broadcast", (data) => {
-
         });
 
         //切断したときに送信
         socket.on("disconnect", () => {
-
+            io.to(roomId).emit('roomPeople', io.sockets.client(roomId).length);  // ルームの参加人数を送信
         });
     });
 }
