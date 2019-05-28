182a0ea10338c61b2769dd946f318cbc8497b4c5
oreotabetai <s17t213@stu.kagawa-u.ac.jp>
2019-05-28 23:10:46 +0900

    制限時間の実装 #51

--- public/javascript/title.js
+++ public/javascript/title.js
@@ -14,9 +14,9 @@ function Click(){
     username = window.prompt("ユーザめいをにゅうりょくしてください");
     if(username === null) return;
     if(username === "") return;
-    
+
     document.body.removeEventListener('click',Click);
-    
+
     popup();
 }
 //ポップアップさせ、ボタンを押させる関数
@@ -35,15 +35,15 @@ function joinClick(){
 }
 //ページ遷移関数
 function postForm(value, nextScreen) {
-    
+
     let form = document.createElement('form');
     let request = document.createElement('block');
-    
+
     form.method = 'POST';
     form.action = '/'+ nextScreen;
     console.log(username);
 
-  
+
     request.type = 'hidden'; //入力フォームが表示されないように
     request.name = 'text';
     request.value = value;
