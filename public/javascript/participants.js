cbe65983a7676186feb1c963e6ac9edf2d578eaf
oreotabetai <s17t213@stu.kagawa-u.ac.jp>
2019-05-28 09:06:19 +0900

    getFormの実装 #35

--- public/javascript/participants.js
+++ public/javascript/participants.js
@@ -12,16 +12,36 @@ send.addEventListener('click', sendForm);
 function sendForm(){
     let roomId = document.getElementById('roomId').value;
     console.log(roomId);
-    postForm(roomId, participatsWaitRoom);
+    getForm(roomId, participatsWaitRoom);
 }
 
-//ページ遷移関数
+//ページ遷移関数(post) 
 function postForm(value, nextScreen) {
     
     let form = document.createElement('form');
     let request = document.createElement('block');
     
     form.method = 'POST';
+    form.action = '/'+ nextScreen;
+
+
+  
+    request.type = 'hidden'; //入力フォームが表示されないように
+    request.name = 'text';
+    request.value = value;
+
+    form.appendChild(request);
+    document.body.appendChild(form);
+    form.submit();
+}
+
+//ページ遷移関数(get)
+function getForm(value, nextScreen) {
+    
+    let form = document.createElement('form');
+    let request = document.createElement('block');
+    
+    form.method = 'GET';
     form.action = '/'+ nextScreen;
 
 
