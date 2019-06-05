312ec69757eb9cdcf0f2e0cc681f2c0f4dbc2ed1
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2019-06-06 00:35:27 +0900

    接続失敗時の例外処理を追加 #71

--- db/room.js
+++ db/room.js
@@ -1,5 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./db/jajanken.db');
+const db = new sqlite3.Database('./db/jajanken.db', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+});
 
 const init = () => {
     db.run(`
