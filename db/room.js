b48203e8ba0f8dcb169ac252ed012f147bece70b
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2019-06-06 00:09:37 +0900

    主キーにNOT NULLを追加した #71

--- db/room.js
+++ db/room.js
@@ -4,7 +4,7 @@ const db = new sqlite3.Database('./db/jajanken.db');
 const init = () => {
     db.run(`
         CREATE TABLE IF NOT EXISTS room (
-            id TEXT PRIMARY KEY,
+            id TEXT NOT NULL PRIMARY KEY,
             master TEXT NOT NULL,
             num INTEGER NOT NULL,
             maxnum INTEGER NOT NULL
