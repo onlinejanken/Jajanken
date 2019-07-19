6084e762856447767fb5e6e63442216f76bfa53b
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2019-07-19 17:23:07 +0900

    勝ち負けの判定を表示する処理を実装 #116

--- controller/roomsController.js
+++ controller/roomsController.js
@@ -6,8 +6,16 @@ router.post('/', (req, res) => {
     res.render('room', { session: req.session });
 });
 
+router.post('/wait', (req, res) => {
+    res.render('participantsWaitRoom', { session: req.session });
+});
+
 router.post('/results', (req, res) => {
-    req.session.resultData = JSON.parse(req.body.resultData);
+    const resultData = JSON.parse(req.body.resultData)
+    if (resultData.judge == 0) {
+        res.render('room', { session: req.session });
+    }
+    req.session.resultData = resultData;
 
     res.render('roomResult', { session: req.session });
 });
