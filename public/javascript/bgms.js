87d5b386a15e7a52dbec849c30ce5e4d487a4105
oreotabetai <s17t213@stu.kagawa-u.ac.jp>
2019-09-18 20:11:54 +0900

    bgm画面の追加 #150

---
+++ public/javascript/bgms.js
@@ -0,0 +1,10 @@
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+
+//読み込み時に音楽を流す
+window.onload = function() {
+    let bgm = new Audio();
+    bgm.src = '/public/bgm/maoudamashii_menu.mp3';
+    bgm.play();
+    bgm.loop = true;
+};
